e53fb0cf00aa8c7ceab86ebc49d3f090e6c55c64
Hugo Munoz <hugo.munoz@epitech.eu>
2026-04-15 16:38:22 +0200

    feat: integrate space-invaders source code and fix lint warnings

---
+++ SpaceInvaders/eslint.config.js
@@ -0,0 +1,22 @@
+import tsParser from '@typescript-eslint/parser';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+
+export default [
+  {
+    files: ['src/**/*.ts'],
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+    },
+    rules: {
+      'no-unused-vars': ['warn', {
+        "argsIgnorePattern": "^_",
+        "varsIgnorePattern": "^_"
+      }],
+    },
+  },
+];
